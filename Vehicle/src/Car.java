6a2c25d2d2e46c825f2d1d66af12c8c59cdff10d
sysadmin <sysadmin@bcalab1-m09>
2021-04-08 08:42:52 +0530

    Method to change speed is created

--- Vehicle/src/Car.java
+++ Vehicle/src/Car.java
@@ -1,10 +1,18 @@
 
 public class Car {
+	
 	String brand;
 	int speed;
+	
 	Car (String brand, int gears)
 	{
 	this.brand = brand;
 	this.speed = speed;
 	}
+	
+	void change_speed(int speed)
+	{
+	this.speed = speed;
+	}
+	
 }
